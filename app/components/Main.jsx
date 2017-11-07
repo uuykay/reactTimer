e51ee03fb8d46ca5c85b0bbdc6bef250522a0801
Will <william.y.kuang@gmail.com>
2017-11-08 00:04:22 +1100

    Initial boilerplate

---
+++ app/components/Main.jsx
@@ -0,0 +1,20 @@
+/*jshint node: true*/
+var React = require('react');
+
+var Main = (props) => {
+	return(
+		<div>
+			<div>
+				<div>
+					<p>Main.jsx. Rendered</p>
+					{props.children}
+				</div>
+			</div>
+			
+			
+			
+		</div>
+	);
+}
+
+module.exports = Main;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# React Timer Application
+
+An exercise in learning the React framework.
