4a53154dd37f64b38f1f3d13e7c598797ddd3c79
Will <william.y.kuang@gmail.com>
2017-11-13 22:32:43 +1100

    Add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -15,6 +17,10 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
+			{/* If it matches slash + countdown */}
+			<Route path="countdown" component={Countdown}/>
+			{/* If it matches nothing, render the Timer component */}
+			<IndexRoute component={Timer}/>
 		</Route>
 	</Router>,
 	document.getElementById('app')
